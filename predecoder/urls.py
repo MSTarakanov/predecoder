a7ff0449330744181cabb5d2adf3b4d14c61da45
Maxim Tarakanov <mstarakanov@yandex.ru>
2021-08-28 21:54:18 +0300

    added tempalte for x86

--- predecoder/urls.py
+++ predecoder/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mips.urls'))
+    path('mips/', include('mips.urls')),
+    path('', include('mainscreen.urls')),
+    path('x86/', include('x86.urls')),
 ]
--- x86/urls.py
+++ x86/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='mips_main')
+    path('', index, name='x86_main')
 ]
