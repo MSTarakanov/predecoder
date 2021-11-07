b8e58deedd2a95ae503a479aaf125679e375501f
Maxim Tarakanov <mstarakanov@yandex.ru>
2021-11-07 16:07:10 +0300

    added desc

--- x86/views.py
+++ x86/views.py
@@ -57,9 +57,11 @@ def index(request, example_id):
 
 
     try:
-        example = examples[example_id - 1]
+        example = examples[example_id - 1]['example']
+        example_desc = examples[example_id - 1]['desc']
     except:
-        example = examples[0]
+        example = examples[0]['example']
+        example_desc = examples[0]['desc']
 
     for command_desc in example:
         stream.append(command_desc['byte'])
@@ -96,64 +98,52 @@ def index(request, example_id):
         'stream_begin': stream_begin,
         'stream_selected': stream_selected,
         'stream_end': stream_end,
+        'example_desc': example_desc,
     }
     return render(request, 'x86/x86_main.html', context)
 
 examples = [
-            [{'byte': '66A100403000', 'description': [['Prefix', '0x66', '8 бит'], ['Opcode', '0xA1', '8 бит'], ['Displacement', '0x00403000', '32 бит']], 'command': 'MOV AX, U'},
-            {'byte': '66F72D004030A1', 'description': [['Prefix', '0x66', '8 бит'],['Opcode', '0x89', '8 бит'], ['immediate', '0x00032', '16 бит']], 'command': 'IMUL V'},
-            {'byte': '6689C3', 'description': [['Prefix', '0x66', '8 бит'],['Opcode', '0x89', '8 бит'], ['Mod R/M', '0xC3', '16 бит']], 'command': 'MOV BX, AX'},
-            {'byte': '66A10040303F', 'description': [['Prefix', '0x66', '8 бит'], ['Opcode', '0xA1', '8 бит'], ['Displacement', '0x00603000', '32 бит']], 'command': 'MOV AX, Z'},
-            {'byte': '6629D8', 'description': [['opcode', '0x2B', '8 бит'], ['immediate', '0x00032', '16 бит']], 'command': 'SUB AX, BX'},
-            {'byte': '66F73D0040307D', 'description': [['opcode', '0x2B', '8 бит'], ['immediate', '0x00032', '16 бит']], 'command': 'IDIV W'},
-            {'byte': '66030500403AFF', 'description': [['opcode', '0x2B', '8 бит'], ['immediate', '0x00032', '16 бит']], 'command': 'ADD AX, Y'},
-            {'byte': '66A300402000', 'description': [['opcode', '0x2B', '8 бит'], ['immediate', '0x00032', '16 бит']], 'command': 'MOV X, AX'}],
+    {'name': 'Выражение','desc': 'Так как мы используем регистры 32-битной разрядности, в составе каждой инструкции есть префикс х66, который указывает на это.' ,'example':
+   [{'byte': '66A100403000', 'description': [['Prefix', '0x66'], ['Opcode', '0xA1'], ['Displacement', '0x00403000']], 'command': 'MOV AX, U'},
+    {'byte': '66F72D004030A1', 'description': [['Prefix', '0x66'],['Opcode', '0xF7'],['Mod R/M', '0x2D'], ['Displacement', '0x00403000']], 'command': 'IMUL V'},
+    {'byte': '6689C3', 'description': [['Prefix', '0x66'],['Opcode', '0x89'], ['Mod R/M', '0xC3']], 'command': 'MOV BX, AX'},
+    {'byte': '66A10040303F', 'description': [['Prefix', '0x66'], ['Opcode', '0xA1'], ['Displacement', '0x0040303F']], 'command': 'MOV AX, Z'},
+    {'byte': '6629D8', 'description': [['Prefix', '0x66'],['Opcode', '0x29'], ['Mod R/M', '0xD8']], 'command': 'SUB AX, BX'},
+    {'byte': '66F73D0040307D', 'description': [['Prefix', '0x66'],['Opcode', '0xF7'],['Mod R/M', '0x3D'], ['Displacement', '0x0040307D']], 'command': 'IDIV W'},
+    {'byte': '66030500403AFF', 'description': [['Prefix', '0x66'],['Opcode', '0x03'],['Mod R/M', '0x05'], ['Displacement', '0x00403AFF']], 'command': 'ADD AX, Y'},
+    {'byte': '66A300402000', 'description': [['Prefix', '0x66'], ['Opcode', '0xA3'], ['Displacement', '0x00402000']], 'command': 'MOV X, AX'}]},
 
-    [{'byte': '33F6', 'description': [['Opcode', '0x33', '8 бит'], ['Mod R/M', '0xF6', '8 бит']],
-      'command': 'XOR ESI, ESI'},
-     {'byte': 'A100403000', 'description': [['Opcode', '0xA1', '8 бит'], ['Displacement', '0x00403000', '32 бит']],
-      'command': 'MOV EAX, [0x00403000]'},
-     {'byte': '8B1D00403000', 'description': [['Opcode', '0x8B', '8 бит'], ['Mod R/M', '0x1D', '8 бит'],
-                                              ['Displacement', '0x00403000', '32 бит']],
-      'command': 'MOV EBX, [0x00403000]'},
-     {'byte': 'B900403000', 'description': [['Opcode', '0xB9', '8 бит'], ['Immediate', '0x00000003', '32 бит']],
-      'command': 'MOV ECX, 3'},
-     {'byte': 'EB58', 'description': [['Opcode', '0xEB', '8 бит'], ['Displacement', '0x58', '8 бит']],
-      'command': 'JMP [0x58]'},
-     {'byte': '3B8600403000', 'description': [['Opcode', '0x3B', '8 бит'], ['Mod R/M', '0x86', '8 бит'],
-                                              ['Displacement', '0x00403000', '32 бит']],
-      'command': 'CMP EAX, [0x00403000]'},
-     {'byte': '741C', 'description': [['Opcode', '0x74', '8 бит'], ['Displacement', '0x1C', '8 бит']],
-      'command': 'JE [0x1C]'},
-     {'byte': '3B8600403000', 'description': [['Opcode', '0x3B', '8 бит'], ['Mod R/M', '0x86', '8 бит'],
-                                              ['Displacement', '0x00403000', '32 бит']],
-      'command': 'CMP EAX, [0x00403000]'},
-     {'byte': '7C24', 'description': [['Opcode', '0x7C', '8 бит'], ['Displacement', '0x24', '8 бит']],
-      'command': 'JL [0x24]'},
-     {'byte': 'EB00', 'description': [['Opcode', '0xEB', '8 бит'], ['Displacement', '0x00', '8 бит']],
-      'command': 'JMP [0x00]'},
-     {'byte': '3B9E00403000', 'description': [['Opcode', '0x3B', '8 бит'], ['Mod R/M', '0x9E', '8 бит'],
-                                              ['Displacement', '0x00403000', '32 бит']],
-      'command': 'CMP EBX, [0x00403000]'},
-     {'byte': '7412', 'description': [['Opcode', '0x74', '8 бит'], ['Displacement', '0x12', '8 бит']],
-      'command': 'JE [0x12]'},
-     {'byte': '7412', 'description': [['Opcode', '0x74', '8 бит'], ['Displacement', '0x12', '8 бит']],
-      'command': 'JE [0x12]'}],
+     {'name': 'Выражение','desc': 'Подсказка' ,'example':
+    [{'byte': '33F6', 'description': [['Opcode', '0x33'], ['Mod R/M', '0xF6']],'command': 'XOR ESI, ESI'},
+     {'byte': 'A100403000', 'description': [['Opcode', '0xA1'], ['Displacement', '0x00403000']],'command': 'MOV EAX, [0x00403000]'},
+     {'byte': '8B1D00403000', 'description': [['Opcode', '0x8B'], ['Mod R/M', '0x1D'],['Displacement', '0x00403000']],'command': 'MOV EBX, [0x00403000]'},
+     {'byte': 'B900403000', 'description': [['Opcode', '0xB9'], ['Immediate', '0x00000003']],'command': 'MOV ECX, 3'},
+     {'byte': 'EB58', 'description': [['Opcode', '0xEB'], ['Displacement', '0x58']],'command': 'JMP [0x58]'},
+     {'byte': '3B8600403000', 'description': [['Opcode', '0x3B'], ['Mod R/M', '0x86'],['Displacement', '0x00403000']],'command': 'CMP EAX, [0x00403000]'},
+     {'byte': '741C', 'description': [['Opcode', '0x74'], ['Displacement', '0x1C']],'command': 'JE [0x1C]'},
+     {'byte': '3B8600403000', 'description': [['Opcode', '0x3B'], ['Mod R/M', '0x86'],['Displacement', '0x00403000']],'command': 'CMP EAX, [0x00403000]'},
+     {'byte': '7C24', 'description': [['Opcode', '0x7C'], ['Displacement', '0x24']],'command': 'JL [0x24]'},
+     {'byte': 'EB00', 'description': [['Opcode', '0xEB'], ['Displacement', '0x00']],'command': 'JMP [0x00]'},
+     {'byte': '3B9E00403000', 'description': [['Opcode', '0x3B'], ['Mod R/M', '0x9E'],['Displacement', '0x00403000']],'command': 'CMP EBX, [0x00403000]'},
+     {'byte': '7412', 'description': [['Opcode', '0x74'], ['Displacement', '0x12']],'command': 'JE [0x12]'},
+     {'byte': '7412', 'description': [['Opcode', '0x74'], ['Displacement', '0x12']],'command': 'JE [0x12]'}]},
 
-[{'byte': 'D90500402000', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b1', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b000', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00402000', '32 бит']], 'command': 'FLD [0x00402000]'},
-            {'byte': 'D80D00402000', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b0', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b001', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00402000', '32 бит']], 'command': 'FMUL [0x00402000]'},
-            {'byte': 'D90500402004', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b1', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b000', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00402004', '32 бит']], 'command': 'FLD [0x00402004]'},
-            {'byte': 'D80D00402004', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b0', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b001', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00402004', '32 бит']], 'command': 'FMUL [0x00402004]'},
-            {'byte': 'DEC1', 'description': [['Opcode', '0xDE', '8 бит'], ['Mod R/M', '0xC1', '8 бит']], 'command': 'FADDP ST(1), ST'},
-            {'byte': 'D9FA', 'description': [['Opcode', '0xD9', '8 бит'], ['Mod R/M', '0xFA', '8 бит']], 'command': 'FSQRT ST'},
-            {'byte': 'DB3D00402008', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b01', '2 бита'], ['Opcode A', '0b1', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b111', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00403010', '32 бит']], 'command': 'FSTP [0x00402008]'}],
+    {'name': 'Выражение','desc': 'Загружаем вещественные значения и перемножаем с самим собой (а^2 и b^2), складываем два получившихся значения в стеке (а^2 и b^2), находим корень и сохраняем вещественное знаечние по заданному адресу.' ,'example':
+   [{'byte': 'D90500402000', 'description': [['Opcode', '0xD9'], ['Mod R/M', '0x05'], ['Displacement', '0x00402000']], 'command': 'FLD [0x00402000]'},
+    {'byte': 'D80D00402000', 'description': [['Opcode', '0xD8'], ['Mod R/M', '0x0D'], ['Displacement', '0x00402000']], 'command': 'FMUL [0x00402000]'},
+    {'byte': 'D90500402004', 'description': [['Opcode', '0xD9'], ['Mod R/M', '0x05'], ['Displacement', '0x00402004']], 'command': 'FLD [0x00402004]'},
+    {'byte': 'D80D00402004', 'description': [['Opcode', '0xD8'], ['Mod R/M', '0x0D'], ['Displacement', '0x00402004']], 'command': 'FMUL [0x00402004]'},
+    {'byte': 'DEC1', 'description': [['Opcode', '0xDE'], ['Mod R/M', '0xC1']], 'command': 'FADDP ST(1), ST'},
+    {'byte': 'D9FA', 'description': [['Opcode', '0xD9'], ['Mod R/M', '0xFA']], 'command': 'FSQRT'},
+    {'byte': 'DB3D00402008', 'description': [['Opcode', '0xDB'], ['Mod R/M', '0x3D'], ['Displacement', '0x00403010']], 'command': 'FSTP [0x00402008]'}]},
 
-[{'byte': 'D90500403008', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b1', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b000', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00402000', '32 бит']], 'command': 'FLD [0x00403008]'},
-            {'byte': 'D9FE', 'description': [['Opcode', '0xD9', '8 бит'], ['Mod R/M', '0xFE', '8 бит']], 'command': 'FSIN ST'},
-            {'byte': 'D80D00403000', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b0', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b001', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00403000', '32 бит']], 'command': 'FMUL [0x00403000]'},
-            {'byte': 'D80D00403004', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b0', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b001', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00403000', '32 бит']], 'command': 'FMUL [0x00403004]'},
-            {'byte': 'D8350040300C', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b00', '2 бита'], ['Opcode A', '0b0', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b110', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x0040300C', '32 бит']], 'command': 'FDIV [0x0040300C]'},
-            {'byte': 'DB3D00403010', 'description': [['Special code', '0b11011', '5 бит'], ['MF', '0b01', '2 бита'], ['Opcode A', '0b1', '1 бит'], ['Mod', '0b00', '2 бита'], ['Opcode B', '0b111', '3 бита'], ['R/M', '0b101', '3 бита'], ['Displacement', '0x00403010', '32 бит']], 'command': 'FSTP [0x00403010]'}],
+    {'name': 'Выражение','desc': 'Загружаем вещественное число (угол между двумя сторонами), находим синус этого угла, домножаем на первую и на вторую сторону и делим на два. После, выгружаем ответ по заданному адресу из стека.' ,'example':
+   [{'byte': 'D90500403008', 'description': [['Opcode', '0xD9'], ['Mod R/M', '0x05'], ['Displacement', '0x00402000']], 'command': 'FLD [0x00403008]'},
+    {'byte': 'D9FE', 'description': [['Opcode', '0xD9'], ['Mod R/M', '0xFE']], 'command': 'FSIN'},
+    {'byte': 'D80D00403000', 'description': [['Opcode', '0xD8'], ['Mod R/M', '0x0D'], ['Displacement', '0x00403000']], 'command': 'FMUL [0x00403000]'},
+    {'byte': 'D80D00403004', 'description': [['Opcode', '0xD8'], ['Mod R/M', '0x0D'], ['Displacement', '0x00403004']], 'command': 'FMUL [0x00403004]'},
+    {'byte': 'D8350040300C', 'description': [['Opcode', '0xD8'], ['Mod R/M', '0x35'], ['Displacement', '0x0040300C']], 'command': 'FDIV [0x0040300C]'},
+    {'byte': 'DB3D00403010', 'description': [['Opcode', '0xDB'], ['Mod R/M', '0x3D'], ['Displacement', '0x00403010']], 'command': 'FSTP [0x00403010]'}]},
 
 
 ]
